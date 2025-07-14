f1666c993807f73435daad9ade0934af9214a5dd
ElizabethMedvedeva <elizabeth.medvedeva12@gmail.com>
2025-07-14 16:54:14 +0300

    fix: fix loader

--- src/components/card-list/card-list.tsx
+++ src/components/card-list/card-list.tsx
@@ -1,26 +1,10 @@
 import React from 'react';
-import type { CardListState } from '../../types/types';
 import { Card } from '../card/card';
 import type { CardListProps } from '../../types/interfaces';
 
-export class CardList extends React.Component<CardListProps, CardListState> {
-  constructor(props: CardListProps) {
-    super(props);
-    this.state = {
-      animals: props.animalsList,
-      loading: false,
-      error: null,
-    };
-  }
-
-  componentDidUpdate(prevProps: CardListProps): void {
-    if (prevProps.animalsList !== this.props.animalsList) {
-      this.setState({ animals: this.props.animalsList });
-    }
-  }
-
+export class CardList extends React.Component<CardListProps> {
   render() {
-    const { animals, loading } = this.state;
+    const { animalsList, loading } = this.props;
 
     if (loading) {
       return (
@@ -33,9 +17,9 @@ export class CardList extends React.Component<CardListProps, CardListState> {
     return (
       <div>
         <h2 className="text-xl font-bold mb-4">List of items</h2>
-        {animals.length === 0 && <p>No animals found.</p>}
+        {animalsList.length === 0 && <p>No animals found.</p>}
         <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-4">
-          {animals.map((animal) => (
+          {animalsList.map((animal) => (
             <Card key={animal.uid} {...animal} />
           ))}
         </div>
