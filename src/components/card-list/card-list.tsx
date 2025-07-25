597c068b074932a7e45531223f46fcfbb7bbf7a4
ElizabethMedvedeva <elizabeth.medvedeva12@gmail.com>
2025-07-25 16:03:51 +0300

    refactor: rewrote the class component of the card list to the functional one

--- src/components/card-list/card-list.tsx
+++ src/components/card-list/card-list.tsx
@@ -2,39 +2,38 @@ import React from 'react';
 import { Card } from '../card/card';
 import type { CardListProps } from '../../types/types';
 
-export class CardList extends React.Component<CardListProps> {
-  render() {
-    const { animalsList, loading, error } = this.props;
-
-    if (loading) {
-      return (
-        <div
-          data-testid="spinner"
-          className="flex justify-center items-center h-64"
-        >
-          <div className="animate-spin rounded-full h-12 w-12 border-t-4 border-yellow-300 border-solid"></div>
-        </div>
-      );
-    }
-
-    if (error) {
-      return (
-        <div className="p-4 bg-red-100 text-red-700 rounded-md text-center">
-          {error}
-        </div>
-      );
-    }
-
+export const CardList: React.FC<CardListProps> = ({
+  animalsList,
+  loading,
+  error,
+}) => {
+  if (loading) {
     return (
-      <div>
-        <h2 className="text-xl font-bold mb-4">List of items</h2>
-        {animalsList.length === 0 && <p>No animals found.</p>}
-        <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-4">
-          {animalsList.map((animal) => (
-            <Card key={animal.uid} {...animal} />
-          ))}
-        </div>
+      <div
+        data-testid="spinner"
+        className="flex justify-center items-center h-64"
+      >
+        <div className="animate-spin rounded-full h-12 w-12 border-t-4 border-yellow-300 border-solid"></div>
       </div>
     );
   }
-}
+
+  if (error) {
+    return (
+      <div className="p-4 bg-red-100 text-red-700 rounded-md text-center">
+        {error}
+      </div>
+    );
+  }
+  return (
+    <div>
+      <h2 className="text-xl font-bold mb-4">List of items</h2>
+      {animalsList.length === 0 && <p>No animals found.</p>}
+      <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-4">
+        {animalsList.map((animal) => (
+          <Card key={animal.uid} {...animal} />
+        ))}
+      </div>
+    </div>
+  );
+};
