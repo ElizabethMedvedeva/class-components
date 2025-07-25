031c4b4a44a1e54f884eea5f7f146306e4f8b54f
ElizabethMedvedeva <elizabeth.medvedeva12@gmail.com>
2025-07-25 16:56:47 +0300

    refactor: rewrote the class component of the search to the functional one

--- src/components/search/search.tsx
+++ src/components/search/search.tsx
@@ -1,86 +1,85 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { searchRequest } from '../../api/apiClient';
-import type { SearchProps, SearchState } from '../../types/types';
+import type { SearchProps } from '../../types/types';
 
-export class Search extends React.Component<SearchProps, SearchState> {
-  constructor(props: SearchProps) {
-    super(props);
-    this.state = {
-      inputValue: '',
-    };
-  }
+export const Search: React.FC<SearchProps> = ({
+  setLoading,
+  setCardState,
+  setError,
+  onSearch,
+}) => {
+  const [inputValue, setInputValue] = useState('');
 
-  handleChange = (event: React.ChangeEvent<HTMLInputElement>) => {
-    this.setState({ inputValue: event.target.value });
+  const handleChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setInputValue(event.target.value);
   };
 
-  handleSearch = async () => {
-    const trimmedInput = this.state.inputValue.trim();
-    this.props.setLoading(true);
+  const handleSearch = async () => {
+    const trimmedInput = inputValue.trim();
+    setLoading(true);
 
     try {
       localStorage.setItem('searchTerm', trimmedInput);
 
       const animalResponse = await searchRequest(trimmedInput);
-      this.props.setCardState(animalResponse.animals);
+      setCardState(animalResponse.animals);
 
-      if (this.props.onSearch) {
-        this.props.onSearch(trimmedInput);
+      if (onSearch) {
+        onSearch(trimmedInput);
       }
     } catch (error) {
       console.error('Search failed:', error);
-      this.props.setError(
-        'Something went wrong while searching. Please try again later.'
-      );
+      setError('Something went wrong while searching. Please try again later.');
     } finally {
-      this.props.setLoading(false);
+      setLoading(false);
     }
   };
 
-  async componentDidMount(): Promise<void> {
-    const savedTerm = localStorage.getItem('searchTerm')?.trim() || '';
-    this.setState({ inputValue: savedTerm });
-    this.props.setLoading(true);
+  useEffect(() => {
+    const fetchInitialSearch = async () => {
+      const savedTerm = localStorage.getItem('searchTerm')?.trim() || '';
+      setInputValue(savedTerm);
+      setLoading(true);
 
-    try {
-      const animalResponse = await searchRequest(savedTerm);
-      this.props.setCardState(animalResponse.animals);
-    } catch (error) {
-      console.error(error);
-      this.props.setError(
-        'Something went wrong while searching. Please try again later.'
-      );
-    } finally {
-      this.props.setLoading(false);
-    }
-  }
+      try {
+        const animalResponse = await searchRequest(savedTerm);
+        setCardState(animalResponse.animals);
+      } catch (error) {
+        console.error(error);
+        setError(
+          'Something went wrong while searching. Please try again later.'
+        );
+      } finally {
+        setLoading(false);
+      }
+    };
+    fetchInitialSearch();
+  }, [setLoading, setCardState, setError]);
 
-  render() {
-    return (
-      <>
-        <div className="w-full bg-black flex items-center justify-between py-4 px-8 shadow-md">
-          <div className="flex items-center gap-2">
-            <img src="logo.png" alt="logo" className="w-[35px] h-[35px]" />
-            <h2 className="text-yellow-300 text-xl font-bold">Pet Shop</h2>
-          </div>
-
-          <div className="flex items-center gap-4">
-            <input
-              type="text"
-              value={this.state.inputValue}
-              onChange={this.handleChange}
-              placeholder="Find your pet"
-              className="px-3 py-2 border-2 border-yellow-300 bg-black text-yellow-300 rounded-md outline-none placeholder-yellow-400"
-            />
-            <button
-              onClick={this.handleSearch}
-              className="cursor-pointer bg-yellow-300 text-black px-4 py-2 rounded-md font-semibold transition duration-300 hover:bg-black hover:text-yellow-300 hover:border-yellow-300 border-2 border-transparent"
-            >
-              Tap to search
-            </button>
-          </div>
+  return (
+    <>
+      <div className="w-full bg-black flex items-center justify-between py-4 px-8 shadow-md">
+        <div className="flex items-center gap-2">
+          <img src="logo.png" alt="logo" className="w-[35px] h-[35px]" />
+          <h2 className="text-yellow-300 text-xl font-bold">Pet Shop</h2>
         </div>
-      </>
-    );
-  }
-}
+
+        <div className="flex items-center gap-4">
+          <input
+            type="text"
+            value={inputValue}
+            onChange={handleChange}
+            placeholder="Find your pet"
+            className="px-3 py-2 border-2 border-yellow-300 bg-black text-yellow-300 rounded-md outline-none placeholder-yellow-400"
+          />
+          <button
+            onClick={handleSearch}
+            className="cursor-pointer bg-yellow-300 text-black px-4 py-2 rounded-md font-semibold transition duration-300 hover:bg-black hover:text-yellow-300 hover:border-yellow-300 border-2 border-transparent"
+          >
+            Tap to search
+          </button>
+        </div>
+      </div>
+    </>
+  );
+};
