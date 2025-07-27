b647345ce42543b9bc9df2c2b5d7f38482021863
ElizabethMedvedeva <elizabeth.medvedeva12@gmail.com>
2025-07-27 19:16:26 +0300

    feat: chage url to search request

--- src/api/apiClient.ts
+++ src/api/apiClient.ts
@@ -1,10 +1,21 @@
 import type { SearchResponse } from '../types/types';
 
-export const searchRequest = async (title: string): Promise<SearchResponse> => {
-  const result = await fetch('https://stapi.co/api/v1/rest/animal/search', {
+export const searchRequest = async (
+  title: string,
+  pageNumber: number = 1,
+  pageSize: number = 6
+): Promise<SearchResponse> => {
+  const query = new URLSearchParams({
+    pageNumber: pageNumber.toString(),
+    pageSize: pageSize.toString(),
+  });
+  const url = 'https://stapi.co/api/v1/rest/animal/search?' + query.toString();
+  const result = await fetch(url, {
     method: 'POST',
     headers: { 'Content-Type': 'application/x-www-form-urlencoded' },
-    body: new URLSearchParams({ name: title }),
+    body: new URLSearchParams({
+      name: title,
+    }),
   });
   if (!result.ok) throw new Error('Failed to fetch characters');
 
